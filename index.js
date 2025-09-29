93874eac93cc574557ac8ff19ab0be2941c64e72
unknown <sagarsonar2508@gmail.com>
2025-09-29 09:16:22 +0530

    updated app.listen

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use("/", searchRouter);
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
