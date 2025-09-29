f0678f2a53a09ebba5e75b41bf49d4250ad49b7a
unknown <sagarsonar2508@gmail.com>
2025-09-29 13:58:43 +0530

    formating

--- src/routes/search.js
+++ src/routes/search.js
@@ -5,78 +5,80 @@ const buildQuery = require("../lib/queryBuilder");
 
 // Search page (HTML)
 router.get("/search", async (req, res, next) => {
-  try {
-    const q = req.query.q || "";
-    const size = parseInt(req.query.size || "10",10);
-    const page = parseInt(req.query.page || "1",10);
-    const from = (page-1)*size;
+    console.log("Rendering search view");
+    try {
+        const q = req.query.q || "";
+        const size = parseInt(req.query.size || "10", 10);
+        const page = parseInt(req.query.page || "1", 10);
+        const from = (page - 1) * size;
 
-    const language = req.query.language || [];
-    const instruments = req.query.instruments || [];
-    const mood = req.query.mood || [];
-    const themes = req.query.themes || [];
-    const bpm_min = req.query.bpm_min;
-    const bpm_max = req.query.bpm_max;
-    const key = req.query.key;
+        const language = req.query.language || [];
+        const instruments = req.query.instruments || [];
+        const mood = req.query.mood || [];
+        const themes = req.query.themes || [];
+        const bpm_min = req.query.bpm_min;
+        const bpm_max = req.query.bpm_max;
+        const key = req.query.key;
 
-    const body = buildQuery(req.query);
+        const body = buildQuery(req.query);
 
-    const response = await client.search({
-      index: process.env.INDEX,
-      from,
-      size,
-      body,
-    });
+        const response = await client.search({
+            index: process.env.INDEX,
+            from,
+            size,
+            body,
+        });
 
-    res.render("search", {
-      q,
-      results: response.hits.hits,
-      total: response.hits.total.value,
-      page,
-      size,
-      language,
-      instruments,
-      mood,
-      themes,
-      bpm_min,
-      bpm_max,
-      key,
-    });
+        res.render("search", {
+            q,
+            results: response.hits.hits,
+            total: response.hits.total.value,
+            page,
+            size,
+            language,
+            instruments,
+            mood,
+            themes,
+            bpm_min,
+            bpm_max,
+            key,
+        });
 
-  } catch(err){
-    next(err);
-  }
+    } catch (err) {
+        next(err);
+    }
 });
 
 
 
 // Search page (HTML)
 router.get("/", async (req, res, next) => {
-  try {
+    console.log("Rendering front view");
+    try {
 
-    res.render("frontView", {
-    });
+        res.render("frontView", {
+        });
 
-  } catch(err){
-    next(err);
-  }
+    } catch (err) {
+        next(err);
+    }
 });
 
 //get song details
 router.get("/details/:id", async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const response = await client.get({
-        index: process.env.INDEX,
-        id: id
-    });
-    console.log(response._source);
-    res.render("details", {
-        data: response._source
-    });
-  }
-    catch(err){
-    next(err);
+    try {
+        const id = req.params.id;
+        const response = await client.get({
+            index: process.env.INDEX,
+            id: id
+        });
+        console.log(response._source);
+        res.render("details", {
+            data: response._source
+        });
+    }
+    catch (err) {
+        next(err);
     }
 });
 
